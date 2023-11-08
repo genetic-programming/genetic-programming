3aca06b8c187c6de94a5d5573fe2e67a815b4f30
Piotr Karaś <92339606+pepe5p@users.noreply.github.com>
2023-11-08 14:25:08 +0100

    Added fitness function (#9)

--- src/individual.py
+++ src/individual.py
@@ -1,8 +1,9 @@
 import random
+from typing import Callable
 
 from anytree import PreOrderIter
 
-from nodes import LanguageNode, NodeType, GRAMMAR, swap_parents, create_node_random, random_value
+from nodes import LanguageNode, NodeType, GRAMMAR, swap_parents, random_value
 
 
 class Individual(LanguageNode):
@@ -33,6 +34,27 @@ def random_crossover(
     allowed_swaps.add(random_node.node_type)
     
     swap_candidates = PreOrderIter(individual_2, filter_=lambda node: node.node_type in allowed_swaps)
+    if not swap_candidates:
+        return
+        
     node_to_swap: LanguageNode = random.choice(list(swap_candidates))
-    
     swap_parents(random_node, node_to_swap)
+
+
+class Interpreter:
+    def interpret(self, individual: Individual, inputs: list[str]) -> list[str]:
+        return inputs
+
+
+def calculate_fitness(
+    individual: Individual,
+    fitness_function: Callable[[list[str], list[str]], float],
+    input_list: list[list[str]],
+    expected_output_list: list[list[str]],
+) -> float:
+    fitness = 0
+    for inputs, expected_outputs in zip(input_list, expected_output_list):
+        actual_outputs = Interpreter().interpret(individual, inputs=inputs)
+        fitness += fitness_function(actual_outputs, expected_outputs)
+        
+    return fitness
